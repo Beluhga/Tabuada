149661a8fdf561d7c90dec9449990752cf6d21f9
Michael <michaelfernando@live.com>
2022-01-26 02:03:30 -0300

    Jogo de Tabuada

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import TelaInicio from './telas/TelaInicio';
+import TelaTabuada from './telas/TelaTabuada';
+import TelaRespostaCerta from './telas/TelaRespostaCorreta';
+import TelaRespostaErrada from './telas/TelaRespostaErrada';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator screenOptions={{ headerShown: false}}>
+
+    <Stack.Screen name="Inicio" component = { TelaInicio } />
+    <Stack.Screen name="Tabuada" component = { TelaTabuada } />
+    <Stack.Screen name= {"RespostaCorreta"} component= { TelaRespostaCerta} />
+    <Stack.Screen name= {"RespostaErrada"} component= { TelaRespostaErrada} />
+
+    </Stack.Navigator>
+</NavigationContainer>
+);
+
+export default App;
